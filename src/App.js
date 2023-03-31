9b12c2a7b653bd0362097d3eeb7c3fbacb8fbbac
rheefine <rheefine@gmail.com>
2023-03-31 22:51:35 +0900

    readline, print, 비동기 처리(callback) 예시

--- src/App.js
+++ src/App.js
@@ -1,3 +1,16 @@
+/*
+const { Console } = require("@woowacourse/mission-utils");
+
+function test() {
+  Console.readLine("닉네임을 입력해주세요.", (answer) => {
+    console.log(`닉네임: ${answer}`);
+    Console.print("hi");
+  });
+}
+
+test();
+*/
+
 class App {
   play() {}
 }
